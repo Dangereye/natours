e2a08e3e18435fa0e114fd565f50fe5480dc443e
Craig Puxty <craig_puxty@talktalk.net>
2024-03-29 14:51:41 +0000

    Handling PATCH requests.

--- app.js
+++ app.js
@@ -42,6 +42,21 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res
+      .status(404)
+      .json({ status: 'not-found', message: 'ID not found.' });
+  }
+
+  res
+    .status(200)
+    .json({ status: 'success', data: { tour: '<Updated tour here...>' } });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
