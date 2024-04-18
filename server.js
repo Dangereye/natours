a8b839dcf6afbdbb8142e56c3e2b4f8509b71c77
Craig Puxty <craig_puxty@talktalk.net>
2024-04-18 13:46:44 +0100

    Added: simple tour model.

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ mongoose
   .connect(DB)
   .then((connection) => console.log('DB Connection Successful!'));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, require: [true, 'A tour must have a price'] },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
